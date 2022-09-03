c320381d5956f314392d6ab881a87ca78e9e1f47
chourabi-dev <chourabitahers62@gmail.com>
2022-09-03 11:50:25 +0100

    latest

---
+++ day-9 09032022 3h/ttcbeta/src/main/java/com/ttcbeta/TtcbetaApplication.java
@@ -0,0 +1,13 @@
+package com.ttcbeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // spring is trying to activate soem background code
+public class TtcbetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtcbetaApplication.class, args);
+	}
+
+}
---
+++ day-9 09032022 3h/ttcbeta/src/main/java/com/ttcbeta/requests/ProductModel.java
@@ -0,0 +1,46 @@
+package com.ttcbeta.requests;
+
+public class ProductModel {
+
+
+	private String title;
+	
+	private float price;
+	
+	private int quantity;
+	
+	
+	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	public ProductModel() {
+		
+	}
+	
+	
+}
+
